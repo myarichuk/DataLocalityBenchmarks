07547f9bc5b704b2e65f542f93925b8d9d050597
Michael Yarichuk <michael.yarichuk@gmail.com>
2020-02-18 11:06:09 +0200

    Initial commit

---
+++ CppDataLocality/CppDataLocality.cpp
@@ -0,0 +1,60 @@
+﻿// CppDataLocality.cpp : Defines the entry point for the application.
+//
+
+#include <benchmark/benchmark.h>
+#include <array>
+
+using namespace std;
+
+class array2d_benchmark
+{
+private:
+	shared_ptr<vector<vector<unsigned long long>>> array2d;
+	int _array_size;
+public:
+	array2d_benchmark(int array_size) : _array_size(array_size)
+	{		
+		array2d = make_shared<vector<vector<unsigned long long>>>(array_size , vector<unsigned long long> (array_size, 0));
+	}
+	
+	void benchmark_column_first() const
+	{
+		for(unsigned long long y = 0; y < _array_size; y++)
+		for(unsigned long long x = 0; x < _array_size; x++)
+			(*array2d)[x][y] = x + y;
+	}
+
+	void benchmark_row_first() const
+	{
+		for(unsigned long long x = 0; x < _array_size; x++)
+		for(unsigned long long y = 0; y < _array_size; y++)
+			(*array2d)[x][y] = x + y;
+	}
+};
+
+
+static void benchmark_array2d_column_first(benchmark::State& state) {
+  const array2d_benchmark benchmark(state.range_x());
+  for (auto _ : state) {
+    benchmark.benchmark_column_first();
+  }
+}
+
+static void benchmark_array2d_row_first(benchmark::State& state) {
+  const array2d_benchmark benchmark(state.range_x());
+  for (auto _ : state) {
+    benchmark.benchmark_row_first();
+  }
+}
+//16, 512, 4096, 16 * 1024
+BENCHMARK(benchmark_array2d_column_first)->Arg(16);
+BENCHMARK(benchmark_array2d_column_first)->Arg(512);
+BENCHMARK(benchmark_array2d_column_first)->Arg(4096);
+BENCHMARK(benchmark_array2d_column_first)->Arg(16 * 1024);
+
+BENCHMARK(benchmark_array2d_row_first)->Arg(16);
+BENCHMARK(benchmark_array2d_row_first)->Arg(512);
+BENCHMARK(benchmark_array2d_row_first)->Arg(4096);
+BENCHMARK(benchmark_array2d_row_first)->Arg(16 * 1024);
+
+BENCHMARK_MAIN();
---
+++ NetCoreDataLocality/Program.cs
@@ -0,0 +1,36 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Running;
+
+namespace NetCoreDataLocality
+{
+    [HardwareCounters(HardwareCounter.CacheMisses)]
+    public class Program
+    {
+        [Params(16, 512, 4096, 16 * 1024)]
+        public int Size { get; set; }
+
+        private long[,] Array2D;
+
+        [IterationSetup]
+        public void Init() => Array2D = new long[Size, Size];
+
+        [Benchmark]
+        public void Iterate2DArrayColumnFirst()
+        {
+            for (int y = 0; y < Size; y++)
+            for (int x = 0; x < Size; x++)
+                Array2D[x, y] = x + y;
+        }
+
+        [Benchmark]
+        public void Iterate2DArrayRowFirst()
+        {
+            for (int x = 0; x < Size; x++)
+            for (int y = 0; y < Size; y++)
+                Array2D[x, y] = x + y;
+        }
+
+        static void Main(string[] args) => BenchmarkRunner.Run<Program>();
+    }
+}
